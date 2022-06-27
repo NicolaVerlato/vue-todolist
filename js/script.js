f47500250f7e56a882dc51c554d84945aa0de462
NicolaVerlato <nicola.verlato00@gmail.com>
2022-06-27 16:14:19 +0200

    finito, adesso inizio i bonus

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ var app = new Vue(
     {
         el: '#root',
         data: {
+            textNewTodo: '',
+            newTodo: {},
             currentTodo: '',
             todos: [
                 {
@@ -19,7 +21,14 @@ var app = new Vue(
             ],
         },
         methods: {
-            
+            addTodo(todos, newTodo) {
+                if(this.textNewTodo.length > 0) {
+                    newTodo.text = (this.textNewTodo);
+                    newTodo.done = false
+                }
+                todos.push(newTodo);
+                this.textNewTodo = '';
+            },
             removeTodo(currentTodo) {
                 this.todos.splice(currentTodo, 1);
             }
